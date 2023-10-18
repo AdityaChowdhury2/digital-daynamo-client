10b4244eab3bcb4896cab2a78664591c96d1e1a2
AdityaChowdhury2 <adityac5102@gmail.com>
2023-10-18 11:29:08 +0600

    firebase setup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,75 +2,77 @@ import Lottie from 'lottie-react';
 import LoginAnimation from './loginAnimation.json';
 import SocialLogin from '../../components/SocialLogin/SocialLogin';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 const Login = () => {
 	return (
 		<section className="bg-base-100 container  mt-20">
+			<Helmet>
+				<title>Digital Dynamo | Login</title>
+			</Helmet>
 			<div className="flex items-center">
 				<div className="w-1/2">
-					<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-						<div>
-							<div className="flex justify-center w-20">
-								<img
-									src={'https://i.ibb.co/LPv0gD7/digidynamo-Logo.png'}
-									alt=""
-								/>
+					<div className="w-full bg-base-100 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+						<div className="p-6 space-y-4 sm:p-8 min-w-lg">
+							<div className="flex justify-center">
+								<div className="w-20">
+									<img
+										src={'https://i.ibb.co/LPv0gD7/digidynamo-Logo.png'}
+										alt=""
+									/>
+								</div>
 							</div>
-						</div>
-						<div className="w-full bg-base-100 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
-							<div className="p-6 space-y-4 md:space-y-6 sm:p-8 min-w-lg">
-								<h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl ">
-									Sign in to your account
-								</h1>
-								<form className="space-y-4 md:space-y-6" action="#">
-									<div>
-										<label
-											htmlFor="email"
-											className="block mb-2 text-sm font-medium "
-										>
-											Your email
-										</label>
-
-										<input
-											type="email"
-											name="email"
-											id="email"
-											placeholder="name@company.com"
-											className="input input-bordered w-full "
-										/>
-									</div>
-									<div>
-										<label
-											htmlFor="password"
-											className="block mb-2 text-sm font-medium "
-										>
-											Password
-										</label>
-
-										<input
-											type="password"
-											name="password"
-											id="password"
-											placeholder="**********"
-											className="input input-bordered w-full "
-										/>
-									</div>
-
-									<button
-										type="submit"
-										className="w-full  bg-base-200 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-base-300"
+							<h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl ">
+								Sign in to your account
+							</h1>
+							<form className="space-y-4 md:space-y-6" action="#">
+								<div>
+									<label
+										htmlFor="email"
+										className="block mb-2 text-sm font-medium "
 									>
-										Sign in
-									</button>
-									<p className="text-sm font-light ">
-										Don&apos;t have an account yet?{' '}
-										<Link to="/register" className="font-medium">
-											Sign up
-										</Link>
-									</p>
-								</form>
-								<div className="divider">OR</div>
-								<SocialLogin />
-							</div>
+										Your email
+									</label>
+
+									<input
+										type="email"
+										name="email"
+										id="email"
+										placeholder="name@company.com"
+										className="input input-bordered w-full "
+									/>
+								</div>
+								<div>
+									<label
+										htmlFor="password"
+										className="block mb-2 text-sm font-medium "
+									>
+										Password
+									</label>
+
+									<input
+										type="password"
+										name="password"
+										id="password"
+										placeholder="**********"
+										className="input input-bordered w-full "
+									/>
+								</div>
+
+								<button
+									type="submit"
+									className="w-full bg-base-200 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-base-300"
+								>
+									Sign in
+								</button>
+								<p className="text-sm font-light ">
+									Don&apos;t have an account yet?{' '}
+									<Link to="/register" className="font-medium">
+										Sign up
+									</Link>
+								</p>
+							</form>
+							<div className="divider">OR</div>
+							<SocialLogin />
 						</div>
 					</div>
 				</div>
